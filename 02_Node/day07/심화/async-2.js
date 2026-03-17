6702acc596aaf09996e07bc3eee7c6264e49e922
yunhacandy <yunhacandy@gmail.com>
2026-03-17 17:14:10 +0900

    오타 수정

--- 02_Node/day07/심화/async-2.js
+++ 02_Node/day07/심화/async-2.js
@@ -1,8 +1,8 @@
 let likePizza = true;
 
-const pizza = new Promise((resolve, rejexct) => {
+const pizza = new Promise((resolve, reject) => {
   if (likePizza) resolve('피자를 주문합니다.');
-  else rejexct('피자를 주문하지 않습니다.');
+  else reject('피자를 주문하지 않습니다.');
 });
 
 pizza.then((result) => console.log(result)).catch((err) => console.log(err));
