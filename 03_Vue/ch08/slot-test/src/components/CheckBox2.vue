77b759c3bdcf70168451fc2bce6897ebbec70cba
yunhacandy <yunhacandy@gmail.com>
2026-03-26 10:57:12 +0900

    day16 과제

---
+++ 03_Vue/ch08/slot-test/src/components/CheckBox2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="checkbox" 
+        :value="id" 
+        :checked="checked"
+        @change="$emit('check-changed', {id, checked: $event.target.checked })" />
+        <slot>Item</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "CheckBox2",
+    props : ["id", "checked"]
+}
+</script>
