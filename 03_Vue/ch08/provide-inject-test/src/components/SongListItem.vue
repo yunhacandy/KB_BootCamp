998eee0e1462c2124769de1b4b08e6e49336a695
yunhacandy <yunhacandy@gmail.com>
2026-03-26 11:20:03 +0900

    day16 심화 과제

---
+++ 03_Vue/ch08/provide-inject-test/src/components/SongListItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <li>
+        <i :class="song.done ? icons.checked : icons.unchecked">
+    </i>
+        {{song.title}}
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "SongListItem",
+        inject: ["icons"],
+        props: ["song"],
+    }
+</script>
