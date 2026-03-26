d9eb59a931c3694b38c57f18c8ee32b050c717ba
yunhacandy <yunhacandy@gmail.com>
2026-03-26 10:17:06 +0900

    day15 과제

---
+++ 03_Vue/ch07/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>
+        <input type="checkbox" :checked="checked" /> {{id}} - {{name}}
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "CheckboxItem",
+        props: {
+            id: [Number, String],
+            name: String,
+            checked: {
+                type: Boolean,
+                required: false,
+                default: false
+            }
+        },
+    }
+</script>
