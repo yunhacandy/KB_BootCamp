3ae7e873712ecba6724c6c10a03f0783d0e8459e
yunhacandy <yunhacandy@gmail.com>
2026-04-28 13:44:14 +0900

    예외처리 실습 완성

--- 04_Java/src/ExceptionPratice/CardPayment.java
+++ 04_Java/src/ExceptionPratice/CardPayment.java
@@ -1,16 +1,53 @@
 package ExceptionPratice;
 
-public class CardPayment extends Payment{
+public class CardPayment extends Payment {
     private String cardNumber;
     private String cardPassword;
     private int monthlyInstallment;
 
-    public CardPayment(String shopName, String productName, long productPrice, String cardNumber, String cardPassword, int monthlyInstallment) {
+    public CardPayment(String shopName, String productName, long productPrice, String cardNumber, String cardPassword,
+                       int monthlyInstallment) {
         super(shopName, productName, productPrice);
         this.cardNumber = cardNumber;
         this.cardPassword = cardPassword;
         this.monthlyInstallment = monthlyInstallment;
     }
 
-    //GET
+    //get, set 등록
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardPassword() {
+        return cardPassword;
+    }
+
+    public void setCardPassword(String cardPassword) {
+        this.cardPassword = cardPassword;
+    }
+
+    public int getMonthlyInstallment() {
+        return monthlyInstallment;
+    }
+
+    public void setMonthlyInstallment(int monthlyInstallment) {
+        this.monthlyInstallment = monthlyInstallment;
+    }
+
+    public String toString() {
+        return "[ 신용카드 결제 정보 ] \n" + "상품명 : " + shopName + "\n상품명 : " + productName + "\n상품가격 : " + productPrice
+                + "\n신용카드번호 : " + cardNumber + "\n할부개월 : " + monthlyInstallment;
+    }
+
+    public void pay() throws PayException{
+        if((productPrice <= 0) || (monthlyInstallment < 0)){
+            throw new PayException("가격이나 할부 개월 수가 잘못되었습니다.");
+        }else{
+            System.out.println("신용카드가 정상적으로 지불되었습니다.");
+        }
+    }
 }
--- 04_Java/src/ExceptionPratice/CashPayment.java
+++ 04_Java/src/ExceptionPratice/CashPayment.java
@@ -1,10 +1,32 @@
 package ExceptionPratice;
 
-public class CashPayment extends Payment{
-    private String cashReciptNumber;
+public class CashPayment extends Payment {
+    private String cashReceiptNumber;
 
-    public CashPayment(String shopName, String productName, long productPrice, String cashReciptNumber) {
-        super(shopName, productName,productPrice);
-        this.cashReciptNumber = cashReciptNumber;
+    public CashPayment(String shopName, String productName, long productPrice, String cashReceiptNumber) {
+        super(shopName, productName, productPrice);
+        this.cashReceiptNumber = cashReceiptNumber;
+    }
+
+    //get set 등록
+    public String getCashReceiptNumber() {
+        return cashReceiptNumber;
+    }
+
+    public void setCashReceiptNumber(String cashReceiptNumber) {
+        this.cashReceiptNumber = cashReceiptNumber;
+    }
+
+    public String toString() {
+        return "[ 현금 결제 정보 ] \n" + "상품명 : " + shopName + "\n상품명 : " + productName + "\n상품가격 : " + productPrice
+                + "\n현금영수증번호 : " + cashReceiptNumber;
+    }
+
+    public void pay() throws PayException{
+        if(productPrice <= 0){
+            throw new PayException("가격이 잘못되었습니다.");
+        }else{
+            System.out.println("현금이 정상적으로 지불되었습니다.");
+        }
     }
 }
